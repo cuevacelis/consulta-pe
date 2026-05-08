73df52903564f9baaa1623e9f995c373cb02eef0
cuevacelis <cuevacelis@hotmail.com>
2026-05-08 10:35:27 -0500

    update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
-import serverlessExpress from '@codegenie/serverless-express';
-import { Callback, Context, Handler } from 'aws-lambda';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
+import serverlessExpress from "@codegenie/serverless-express";
+import { Context } from "aws-lambda";
+import { AppModule } from "./app.module";
 
-let server: Handler;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let server: any;
 
-async function bootstrap(): Promise<Handler> {
-  const app = await NestFactory.create(AppModule, { logger: ['error', 'warn'] });
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    logger: ["error", "warn"],
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.init();
 
@@ -15,11 +18,7 @@ async function bootstrap(): Promise<Handler> {
   return serverlessExpress({ app: expressApp });
 }
 
-export const handler: Handler = async (
-  event: any,
-  context: Context,
-  callback: Callback,
-) => {
+export const handler = async (event: any, context: Context) => {
   server = server ?? (await bootstrap());
-  return server(event, context, callback);
+  return server(event, context);
 };
